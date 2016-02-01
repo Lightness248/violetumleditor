ef737209e93830ec6e0e6a09f7cff43a238ac275
Lightness248 <spomazany29@gmail.com>
2016-02-02 00:24:28 +0100

    Editied trasition edge in state diagram

---
+++ violet-framework/src/test/java/com/horstmann/violet/product/diagram/abstracts/property/SynchronizationBarOrientationTest.java
@@ -0,0 +1,28 @@
+package com.horstmann.violet.product.diagram.abstracts.property;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Slawomir Pomazany on 2016-02-01.
+ */
+public class SynchronizationBarOrientationTest
+{
+
+    @org.junit.Test
+    public void testIsVertical() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testSetVertical() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testClone() throws Exception
+    {
+
+    }
+}
---
+++ violetplugin-statediagram/src/main/java/com/horstmann/violet/product/diagram/state/StateTransitionEdgeBeanInfo.java
@@ -0,0 +1,8 @@
+package com.horstmann.violet.product.diagram.state;
+
+/**
+ * Created by Slawomir Pomazany on 2016-02-01.
+ */
+public class StateTransitionEdgeBeanInfo
+{
+}
